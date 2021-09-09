5dbff8b0d09e99ca3d5c7ed1a8272d8756b23c49
adugourd <aurelien.dugourd@bioquant.uni-heidelberg.de>
2021-09-09 11:04:18 +0200

    handled missing values

--- R/plasma_contamination.R
+++ R/plasma_contamination.R
@@ -18,30 +18,63 @@
 #' data(test_data)
 #'
 #' conta_scores <- plasma_contamination(test_data)
-plasma_contamination <- function(df, n_perm = 1000, plot = T) {
+plasma_contamination <- function(df, n_perm = 1000, plot = T, verbose = T) {
   if(dim(df)[2] == 1)
   {
+    if(verbose)
+    {
+      print("Computing scores for 1 sample/contrast...")
+    }
+    df <- df[complete.cases(df),,drop = F]
     df$decouplerIsGreat <- df[,1]
+    means <- decoupleR::decouple(mat = as.matrix(df), network = reg_sets, .source = "set", .target = "gene",
+                                 statistics = c("mean"),
+                                 args = list(
+                                   mean = list(.mor = "mor", .likelihood = "likelihood", times=n_perm)
+                                 )
+    )
+    means <- as.data.frame(means)
+    means <- means[grepl("normalized_mean",means$statistic),]
+    means <- means[!grepl("^decouplerIsGreat$",means$condition),]
+    means <- means[,-c(1,2,6)]
+    names(means)[1] <- "set"
+
+    score_df <- means
+  } else
+  {
+    score_list <- list()
+    for(i in 1:length(df[1,]))
+    {
+      if(verbose)
+      {
+        print(paste0("Computing scores for sample ", i, " out of ",length(df[1,]),"..."))
+      }
+      sub_df <- df[,i,drop = F]
+      sub_df <- sub_df[complete.cases(sub_df),,drop = F]
+      sub_df$decouplerIsGreat <- sub_df[,1]
+      means <- decoupleR::decouple(mat = as.matrix(sub_df), network = reg_sets, .source = "set", .target = "gene",
+                                   statistics = c("mean"),
+                                   args = list(
+                                     mean = list(.mor = "mor", .likelihood = "likelihood", times=n_perm)
+                                   )
+      )
+      means <- as.data.frame(means)
+      means <- means[grepl("normalized_mean",means$statistic),]
+      means <- means[!grepl("^decouplerIsGreat$",means$condition),]
+      means <- means[,-c(1,2,6)]
+      names(means)[1] <- "set"
+      score_list[[i]] <- means
+    }
+    score_df <- as.data.frame(do.call(rbind,score_list))
   }
-  means <- decoupleR::decouple(mat = as.matrix(df), network = reg_sets, .source = "set", .target = "gene",
-                              statistics = c("mean"),
-                              args = list(
-                                mean = list(.mor = "mor", .likelihood = "likelihood", times=n_perm)
-                              )
-  )
-  means <- as.data.frame(means)
-  means <- means[grepl("normalized_mean",means$statistic),]
-  means <- means[!grepl("^decouplerIsGreat$",means$condition),]
-  means <- means[,-c(1,2,6)]
-  names(means)[1] <- "set"
-
-  contamination_scores <- means
-
   if(plot)
   {
-    plot(ggplot(contamination_scores, aes(y = score, x = condition, group = set, fill = set)) +
+    plot(ggplot(score_df, aes(y = score, x = condition, group = set, fill = set)) +
            geom_bar(position = "dodge", stat = "identity", color="black") +
-           theme_minimal())
+           theme_minimal() +
+           theme(axis.text.x=element_text(angle = 45, hjust = 1)) +
+           theme(plot.margin = unit(c(1,1,1,2), "cm")))
+
   }
-  return(contamination_scores)
+  return(score_df)
 }
